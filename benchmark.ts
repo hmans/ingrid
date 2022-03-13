12253e8e5bb298b490a3f7bcd97c58f82104d797
Hendrik Mans <hendrik@mans.de>
2022-03-13 18:51:37 +0100

    Benchmark moving entities

--- benchmark.ts
+++ benchmark.ts
@@ -3,11 +3,18 @@ import { table } from "table"
 
 const now = performance.now
 
-/* Add objects */
-const addObjects = (iterations = 1000, numEntities = 10_000) => {
-  /* Create entities */
+type Entity = {
+  id: number
+  position: {
+    x: number
+    y: number
+    z: number
+  }
+}
+
+const makeEntities = (num: number): Entity[] => {
   const entities = []
-  for (let i = 0; i < numEntities; i++) {
+  for (let i = 0; i < num; i++) {
     entities.push({
       id: i,
       position: {
@@ -18,10 +25,17 @@ const addObjects = (iterations = 1000, numEntities = 10_000) => {
     })
   }
 
-  /* Add objects to grid */
+  return entities
+}
+
+const addEntities = (iterations = 1000, numEntities = 10_000) => {
+  /* Create entities */
+  const entities = makeEntities(numEntities)
+  const grid = new BoundlessGrid()
+
   const start = now()
   for (let i = 0; i < iterations; i++) {
-    const grid = new BoundlessGrid()
+    /* Add objects to grid */
     for (const entity of entities) {
       grid.set(entity, entity.position)
     }
@@ -33,6 +47,36 @@ const addObjects = (iterations = 1000, numEntities = 10_000) => {
   return [`Adding ${numEntities} entities:`, time.toFixed(3)]
 }
 
-const results = [addObjects()]
+const moveEntities = (iterations = 1000, numEntities = 10_000) => {
+  /* Create entities */
+  const entities = makeEntities(numEntities)
+  const grid = new BoundlessGrid()
+
+  for (const entity of entities) {
+    grid.set(entity, entity.position)
+  }
+
+  const start = now()
+
+  for (let i = 0; i < iterations; i++) {
+    /* Move and update all entities */
+    for (const entity of entities) {
+      entity.position.x = Math.random() * 1000 - 1000
+      entity.position.y = Math.random() * 1000 - 1000
+      entity.position.z = Math.random() * 1000 - 1000
+      grid.set(entity, entity.position)
+    }
+  }
+  const stop = now()
+  const time = (stop - start) / iterations
+
+  return [`Moving ${numEntities} entities:`, time.toFixed(3)]
+}
+
+const results = [
+  ["Benchmark Name", "Average Time per Iteration (ms)"],
+  addEntities(),
+  moveEntities()
+]
 
 console.log(table(results))
